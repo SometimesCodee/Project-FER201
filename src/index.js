a596820d7966fac5825b51deb91dde4c3a89bb6a
dinh vuong <dinhvuong2311@gmail.com>
2024-07-09 20:28:36 +0700

    redux

--- src/index.js
+++ src/index.js
@@ -4,10 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux';
+import rootReducer from './redux/rootReducer';
+
+const store = createStore(rootReducer)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <React.StrictMode store={store}>
     <App />
   </React.StrictMode>
 );
